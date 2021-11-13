b24454773161e09df45f7519081173373e7fe036
chahir-l <c.loukili@gmail.com>
2021-11-13 15:13:21 +0100

    added new input

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import Input from './Input'
+
+
+function Contact()
+{
+
+    return <div>
+        <label>Nom :</label>
+        <Input />
+    </div>
+}
+
+export default Contact;
---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+
+
+function Input()
+{
+    return <div>
+        <input type="text" name="test" value="" />
+    </div>
+}
+
+export default Input
